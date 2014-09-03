c3397a917d771c046d9af6f35db21edaa4cdcd64
Toshiya SAITOH <stoshiya@gmail.com>
2014-09-03 09:52:50 +0900

    Bump package version.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
